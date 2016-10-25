84e62c51f852a6c882b1e18acfce7f8eed8557f6
pvkrishna0007 <pvkrishna0007@gmail.com>
2016-10-25 23:31:51 +0530

    added weekly courses

---
+++ app/src/main/java/com/tenhrs/courses/BaseCompactActivity.java
@@ -0,0 +1,9 @@
+package com.tenhrs.courses;
+
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Krishna on 10/25/2016.
+ */
+public class BaseCompactActivity extends ActionBarActivity {
+}
---
+++ app/src/main/java/com/tenhrs/courses/WeeklyCourseActivity.java
@@ -0,0 +1,15 @@
+package com.tenhrs.courses;
+
+import android.os.Bundle;
+
+/**
+ * Created by Krishna on 10/25/2016.
+ */
+public class WeeklyCourseActivity extends BaseCompactActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
