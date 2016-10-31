623d354304e73002f7fef5ac83ece6242c7cc501
pvkrishna0007 <pvkrishna0007@gmail.com>
2016-10-31 23:22:49 +0530

    added weekly courses

--- app/src/main/java/com/tenhrs/courses/LoginActivity.java
+++ app/src/main/java/com/tenhrs/courses/LoginActivity.java
@@ -18,6 +18,7 @@ public class LoginActivity extends BaseCompactActivity {
 
         activityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
 
+
     }
 
     @Override
