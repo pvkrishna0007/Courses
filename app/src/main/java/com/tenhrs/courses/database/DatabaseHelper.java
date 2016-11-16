0a9d2569cc194a81e28d4c5cef97f64afd333857
pvkrishna0007 <pvkrishna0007@gmail.com>
2016-11-16 23:37:59 +0530

    Added flash card options and result

--- app/src/main/java/com/tenhrs/courses/database/DatabaseHelper.java
+++ app/src/main/java/com/tenhrs/courses/database/DatabaseHelper.java
@@ -1,59 +1,77 @@
 package com.tenhrs.courses.database;
 
-import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 
 import com.tenhrs.courses.common.Constants;
+
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by SIVA on 27-05-2016.
  */
-public class DatabaseHelper extends SQLiteOpenHelper {
+public class DatabaseHelper {
 
     private static DatabaseHelper mInstance = null;
 
-    private static final String DATABASE_NAME = Constants.DB_NAME;
-    private static final String DATABASE_TABLE = "tableName";
-    private static final int DATABASE_VERSION = 1;
+    private SQLiteDatabase mDatabase;
+    private static DatabaseHelper sDataBaseHandler;
+    private AtomicInteger mOpenCounter = new AtomicInteger();
 
-    private Context context;
 
-    public static DatabaseHelper getInstance(Context ctx) {
-        if (mInstance == null) {
-            mInstance = new DatabaseHelper(ctx.getApplicationContext());
+    public static synchronized DatabaseHelper getInstance() {
+        if (sDataBaseHandler == null) {
+            sDataBaseHandler = new DatabaseHelper();
         }
-        return mInstance;
+        return sDataBaseHandler;
     }
 
-    /**
-     * constructor should be private to prevent direct instantiation.
-     * make call to static factory method "getInstance()" instead.
-     */
-    private DatabaseHelper(Context ctx) {
-        super(ctx, DATABASE_NAME, null, DATABASE_VERSION);
-        this.context = ctx;
+    public synchronized SQLiteDatabase openDatabase() {
+        try {
+            if (mOpenCounter.incrementAndGet() == 1) {
+                if (mDatabase == null) {
+                    mDatabase = SQLiteDatabase.openDatabase(Constants.DB_PATH, null, SQLiteDatabase.OPEN_READWRITE | SQLiteDatabase.NO_LOCALIZED_COLLATORS);
+                } else if (!mDatabase.isOpen()) {
+                    mDatabase = SQLiteDatabase.openDatabase(Constants.DB_PATH, null, SQLiteDatabase.OPEN_READWRITE | SQLiteDatabase.NO_LOCALIZED_COLLATORS);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+
+
+        }
+        return mDatabase;
     }
 
-    // Creating Tables
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE \"Courses\" (\"CourseID\" INTEGER PRIMARY KEY  NOT NULL ,\"CourseName\" TEXT, \"isActive\" INTEGER)");
-
+    public synchronized void closeDatabase() {
+        if (mOpenCounter.decrementAndGet() == 0) {
+            if (mDatabase != null) {
+                mDatabase.close();
+            }
+        }
     }
 
-    // Upgrading database
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        // Drop older table if existed
-//        db.execSQL("DROP TABLE IF EXISTS " + QUESTIONS);
+    public ArrayList<String> pragmainfo(String tablename) {
+        ArrayList<String> pragma = new ArrayList<String>();
+        openDatabase();
+        String pragmaquery = "pragma table_info(" + tablename + ")";
+        Cursor mCursor = mDatabase.rawQuery(pragmaquery, null);
+        try {
 
-        // Create tables again
-//        onCreate(db);
-    }
+            if (mCursor.moveToFirst()) {
+                do {
+                    pragma.add(mCursor.getString(1));
 
-    public void insertCoursesData() {
+                } while (mCursor.moveToNext());
+            }
 
+        } catch (Exception e) {
+        } finally {
+            closeDatabase();
+        }
+
+        return pragma;
     }
 
 }
